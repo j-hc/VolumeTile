282da234c9e9321486d86a3238a9a5868d97ee20
j-hc <j-hc@users.noreply.github.com>
2023-03-01 15:40:38 +0300

    volume tile

---
+++ app/src/main/java/com/jhc/volumetile/VolumeTileService.kt
@@ -0,0 +1,18 @@
+package com.jhc.volumetile
+
+import android.media.AudioManager
+import android.service.quicksettings.TileService
+
+class VolumeTileService : TileService() {
+    private lateinit var am: AudioManager
+
+    override fun onStartListening() {
+        am = getSystemService(AUDIO_SERVICE) as AudioManager
+    }
+
+    override fun onClick() {
+        am.adjustStreamVolume(
+            AudioManager.STREAM_MUSIC, AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI
+        )
+    }
+}
